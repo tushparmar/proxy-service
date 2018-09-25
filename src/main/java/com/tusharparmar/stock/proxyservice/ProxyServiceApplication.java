2a0932e25d01680fe950ba8a51e78a0bc2a8f4cb
Tushar Parmar <tparmar@fulcrum-gt.com>
2018-09-25 12:42:36 -0500

    Zuul proxy implemented.

--- src/main/java/com/tusharparmar/stock/proxyservice/ProxyServiceApplication.java
+++ src/main/java/com/tusharparmar/stock/proxyservice/ProxyServiceApplication.java
@@ -2,7 +2,9 @@ package com.tusharparmar.stock.proxyservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ProxyServiceApplication {
 
